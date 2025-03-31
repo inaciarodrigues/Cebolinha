69ec4a2af2caf9f97acdafc4795e93fcf6804e0f
inaciarodrigues <soterinacia688@gmail.com>
2025-03-31 10:39:43 -0300

    codigo do exercicio

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿string FraseDigitada;
+string LetrasTrocadas;
+
+Console.Write("Digite uma frase: ");
+FraseDigitada = Console.ReadLine()!;
+
+LetrasTrocadas = FraseDigitada.Replace ("r", "l").Replace ("R", "L");
+
+Console.WriteLine(LetrasTrocadas);
